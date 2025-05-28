5bc94fb990f1858cd587ccd5c3613c7362196c7c
吴纪曙 <wujishu1@163.com>
2025-05-28 15:50:21 +0000

    update vite.config.js. 修改为不压缩代码，增加base路径“./”
    Signed-off-by: 吴纪曙 <wujishu1@163.com>

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,29 @@
-import { defineConfig } from 'vite'
+// import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vite.dev/config/
+// // https://vite.dev/config/
+// export default defineConfig({
+//     base: './',
+//   plugins: [vue()],
+// })
+// vite.config.js 或 vite.config.ts
+import { defineConfig } from 'vite';
 export default defineConfig({
-  plugins: [vue()],
-})
+    base: './',
+    plugins: [vue()],
+    build: {
+        // 关闭代码压缩
+        rollupOptions: {
+            // 配置输出格式，'es' 为 ES module，'cjs' 为 CommonJS
+            output: {
+                format: 'es',
+                // 不生成sourcemap
+                sourcemap: false,
+            },
+        },
+        // 关闭terser压缩
+        terserOptions: false,
+        // 关闭兼容性填充代码（polyfill）
+        polyfillModulePreload: false,
+    },
+});
